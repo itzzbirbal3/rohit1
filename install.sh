efee11bbf48c50d0f996c02d08e9275460b8348e
rishabguta <rishab.ddos@gmail.com>
2024-09-28 17:14:31 +0530

    Update and rename install (1).sh to install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/bot/soul.py",
+    "postStartCommand": "python3 /workspaces/rohit1/soul.py",
     "customizations": {
         "vscode": {
             "settings": {
